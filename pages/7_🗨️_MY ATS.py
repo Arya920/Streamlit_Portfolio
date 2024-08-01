fe83e9eb86f8e2e2375f07db6c3452fe5f55c48c
Arya920 <aryachakraborty.official@gmail.com>
2024-08-01 12:02:02 +0530

    ATS

--- pages/7_🗨️_MY ATS.py
+++ pages/7_🗨️_MY ATS.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import re
 import json
-from Information_Extraction import extract_information
+
 from functions import DonutChart
 st.markdown(" # My Customized ATS")
+
+st.warning("""This System only works on local platform as it uses LLAMA CPP which is not designed to be deployedso for that reason to use this ATS you have to fork this repository and follow the below process. """)
 
 st.warning("""To run This Web based ATS Checker I have used an open sourced Information Extraction model called **:green[`NuMind/NuExtract`]**. 
            Please Download the model **:green[`NuExtract-Q4_K_L.gguf`]** at first and keep that model in the **:green[`models`]** folder. Use the following link 👇""")
@@ -84,54 +86,59 @@ st.markdown(
 jd = st.text_area("Copy and Paste yor Job Description here")
 word_count_text = f"Word count: {word_count(jd)} / 512"
 st.caption(word_count_text)
-
-
-if 'jd_information' not in st.session_state:
-    st.session_state.jd_information = {}
-
-if 'percentage' not in st.session_state:
-    st.session_state.percentage = ''
-
 submit_button = st.button("Submit")
 
-if submit_button:
-    if jd !="":
-        jd = re.sub(r'\n', '',jd)
-        if word_count(jd) <= 512:
-            #global jd_information
-            jd_information = extract_information( jd, example=["","",""])
 
-            print('jd👇',jd_information)
-            
-            jd_information = json.loads(jd_information)
-            st.session_state.jd_information = jd_information
+try:
+
+    from Information_Extraction import extract_information
+
+    if 'jd_information' not in st.session_state:
+        st.session_state.jd_information = {}
+
+    if 'percentage' not in st.session_state:
+        st.session_state.percentage = ''
+
+    if submit_button:
+        if jd !="":
+            jd = re.sub(r'\n', '',jd)
+            if word_count(jd) <= 512:
+                #global jd_information
+                jd_information = extract_information( jd, example=["","",""])
+
+                print('jd👇',jd_information)
+                
+                jd_information = json.loads(jd_information)
+                st.session_state.jd_information = jd_information
 
 
-            matched, missing, percentage = skill_match_analysis(my_resume_key_points, jd_information)
+                matched, missing, percentage = skill_match_analysis(my_resume_key_points, jd_information)
 
-            st.session_state.percentage = round(percentage, 2)
-            #st.write("Programming Skills👉",jd_information['Programming Languages'])
-            #st.write("Technical Skills👉",jd_information['Technical Skills'])
-            #st.write("Soft Skills👉",jd_information['Soft Skills'])
-            
-        else:
-            st.warning("Word count exceeds the limit of 670 words. Please shorten your input.")
-
-    col1, col2 = st.columns([3,2])
-    with col1:
-        
-        DonutChart.doughnut_plot(float(st.session_state.percentage))
-    with col2:
-        if st.session_state.jd_information:
-            if 'Programming Languages' in st.session_state.jd_information:
-                st.write("Programming Skills👉", st.session_state.jd_information['Programming Languages'])
+                st.session_state.percentage = round(percentage, 2)
+                #st.write("Programming Skills👉",jd_information['Programming Languages'])
+                #st.write("Technical Skills👉",jd_information['Technical Skills'])
+                #st.write("Soft Skills👉",jd_information['Soft Skills'])
+                
             else:
-                st.write("Programming Skills: Not specified in the job description")
+                st.warning("Word count exceeds the limit of 670 words. Please shorten your input.")
 
-            if 'Technical Skills' in st.session_state.jd_information:
-                st.write("Technical Skills👉", st.session_state.jd_information['Technical Skills'])
+        col1, col2 = st.columns([3,2])
+        with col1:
+            
+            DonutChart.doughnut_plot(float(st.session_state.percentage))
+        with col2:
+            if st.session_state.jd_information:
+                if 'Programming Languages' in st.session_state.jd_information:
+                    st.write("Programming Skills👉", st.session_state.jd_information['Programming Languages'])
+                else:
+                    st.write("Programming Skills: Not specified in the job description")
+
+                if 'Technical Skills' in st.session_state.jd_information:
+                    st.write("Technical Skills👉", st.session_state.jd_information['Technical Skills'])
+                else:
+                    st.write("Technical Skills: Not specified in the job description")
             else:
-                st.write("Technical Skills: Not specified in the job description")
-        else:
-            st.write("Please submit a job description to see the required skills.")
+                st.write("Please submit a job description to see the required skills.")
     
+except Exception as e:
+    st.warning('This is still under development. If you know how to deploy Llama CPP on cloud please let me know at my mail id: aryachakraborty.official@gmail.com')
